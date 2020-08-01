48b2382baa53828f140508c06a556557f6d4d7f0
nodokaodayaka <wankosob@gmail.com>
2020-08-01 11:47:52 +0900

    max length check modify

--- src/components/FizzBuzzList.tsx
+++ src/components/FizzBuzzList.tsx
@@ -4,10 +4,6 @@ import { FizzBuzz } from './FizzBuzz'
 export const FizzBuzzList = (props: any) => {
   const { num } = props
 
-  if (num.length > 1) {
-    return <div>一桁までです。</div>
-  }
-
   let list = [...Array(Number(num))].map((_, i) => i + 1)
 
   return (
